0242d74a0eebd9c3edf99231f8462aa8fff7e715
sajjadAtlas <80490489+sajjadAtlas@users.noreply.github.com>
2024-10-17 17:03:25 -0400

    Update AUIPCUnit.v
    added flag ti check if auipc instruction happened so auipc doesn't trigger every cycle

--- AUIPCUnit.v
+++ AUIPCUnit.v
@@ -1,12 +1,18 @@
 module auipc_unit (
-    input [31:0] currentPC,
-    input [31:0] imm,
-    output reg [31:0] updatedPC
+    input isAUIPC,            
+    input [31:0] currentPC,   
+    input [31:0] imm,         
+    output reg [31:0] updatedPC  
 );
 
-    always@(*)
-    begin
-        updatedPC <= currentPC + (imm << 12);
+    always@(*) begin
+        if (isAUIPC) begin
+            // AUIPC: Add immediate (shifted by 12) to the current PC
+            updatedPC <= currentPC + (imm << 12);
+        end
+        else begin
+            updatedPC <= currentPC; // No change if not AUIPC
+        end
     end
     
 endmodule
